da6bd4cc23384e071931a239f4c37cce5190c89f
KAKIDUKA <hqkps0609@gmail.com>
2022-11-21 16:53:22 +0900

    課題提出ファイル

--- PHPjob/3-1/Exercises.php
+++ PHPjob/3-1/Exercises.php
@@ -5,15 +5,13 @@ $products = ["鉛筆" => 100, "消しゴム" => 150, "物差し" => 500];
 
 function include_tax($price) {
     //鉛筆
-    return $price = 100 * TAX;
-    //消しゴム
-    return $price = 150 * TAX;
-    //物差し
-    return $price = 500 * TAX;
+    return $price * TAX;
 }
 
 foreach($products as $key => $price) {
-    echo $key. "の税込価格は" .$price. "円です";
+    print $key. "の税込価格は" ;
+    print include_tax($price);
+    print "円です";
     echo '<br>';
 }
 ?>
